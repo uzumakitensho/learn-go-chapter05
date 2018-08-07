b6b44d75c814738523c657bbb40cf2dc667d72c2
Hafid Afridian <afridianhafid@gmail.com>
2018-08-07 14:09:19 +0700

    add protocol buffer with go

---
+++ protobufs/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"github.com/golang/protobuf/proto"
+	pb "github.com/uzumakitensho/chapter05/protofiles"
+)
+
+func main() {
+	p := &pb.Person{
+		Id:    1234,
+		Name:  "Roger F",
+		Email: "rf@example.com",
+		Phones: []*pb.Person_PhoneNumber{
+			{Number: "555-4321", Type: pb.Person_HOME},
+		},
+	}
+
+	p1 := &pb.Person{}
+	body, _ := proto.Marshal(p)
+	_ = proto.Unmarshal(body, p1)
+	fmt.Println("Original struct loaded from proto file:", p, "\n")
+	fmt.Println("Marshaled proto data: ", body, "\n")
+	fmt.Println("Unmarshaled struct: ", p1)
+}
